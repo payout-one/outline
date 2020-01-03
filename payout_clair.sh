dac7073562ded3520e73569d2d24eb904f08e7f4
Oliver Kriska <oliver.kriska@payout.one>
2020-01-03 12:38:15 +0100

    Semaphore - fixed clair

--- payout_clair.sh
+++ payout_clair.sh
@@ -6,18 +6,23 @@
 # - change script for IP checking
 # - make one bin file with all scripts
 # - auto detect OS and choose script
-IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' -m 1)
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Darwin*)    IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' -m 1);;
+    *)          IP=172.17.0.1
+esac
 
 docker stop db clair &>/dev/null && docker rm db clair &>/dev/null
 docker run -d --name db arminc/clair-db:latest
-sleep 1
-docker run -p 6060:6060 --link db:postgres -d --name clair arminc/clair-local-scan:v2.0.6
-sleep 1
-# docker pull $1
+sleep 5
 
-./clair-scanner_linux_amd64 --ip $IP --all $1 &> results.txt
-# For mac change to this script
-# ./clair-scanner_darwin_amd64 --ip $IP --all $1 &> results.txt
+docker run -p ${IP}:6060:6060 --link db:postgres -d --name clair arminc/clair-local-scan:v2.0.6
+sleep 5
+
+case "${unameOut}" in
+    Darwin*)    ./clair-scanner_darwin_amd64 -c="http://${IP}:6060" --ip "${IP}" --all $1 &> results.txt;;
+    *)          ./clair-scanner_linux_amd64 -c="http://${IP}:6060" --ip="${IP}" --all $1 &> results.txt
+esac
 
 cat results.txt
 docker stop db clair &>/dev/null && docker rm db clair &>/dev/null
